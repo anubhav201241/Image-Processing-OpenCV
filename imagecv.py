12f47e4a45e128536ffd320650f3b0bbf0baed62
anubhav201241 <72249059+anubhav201241@users.noreply.github.com>
2021-03-09 01:12:39 +0530

    Create imagecv.py
    In this it is showing how we take a image and can view it in as RGB form, This can make the image aspect more clearly to understand.

---
+++ imagecv.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np   
+img = cv2.imread('elon.jpg')  #Download picture and paste the format of that image(jpeg,png or jpg) 
+img = cv2.resize(img, None , fx=0.5, fy=0.5)  
+red_channel = img[:, :, 0]
+blue_channel = img[:, :, 1]
+green_channel = img[:, :, 2]
+#Create Empty image through Numpy Array - every Pixel Value
+
+red_img = np.zeros(img.shape)
+blue_img = np.zeros(img.shape)
+green_img = np.zeros(img.shape)
+                                                                    #print(img.shape)
+                                                                    #open Cv reads an image as B, G, R   //realimage.py
+                                                                    #red_channel = img[:, :, 2]
+                                                                    #Create  empty iimage thorugh empty array evry pixel in this line of code is 0
+                                                                    #red_img = np.zeros(img.shape)
+                                                                    #print(red_img)
+                                                                    #Assigning values from red channel to red image
+                                                                    #red_img[:, :, 2] = red_channel
+
+red_img[:, :, 0] = red_channel
+blue_img[:, :, 1] = blue_channel
+green_img[:, :, 2] = green_channel
+
+#isolated red calue of the image 
+cv2.imshow('Red Channel',red_img)
+cv2.imshow('Blue channel',blue_img)
+cv2.imshow('Green Channel', green_img)
+cv2.waitKey()
